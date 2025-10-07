73c6db2f5a092b1fb3b8d174cbacfeef961894f2
Vignesh Sekar <vigneshsekar1996@gmail.com>
2025-10-07 19:36:14 +0530

    added a file server

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
 	httpServe := http.Server{Handler: serveMux, Addr: ":8080"}
 	if err := httpServe.ListenAndServe(); err != nil {
 		log.Fatal(err)
