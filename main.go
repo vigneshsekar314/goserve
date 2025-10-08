df312a5da3d1472cbba10157b1044d5990aaf8f4
Vignesh Sekar <vigneshsekar1996@gmail.com>
2025-10-08 15:33:46 +0530

    added metrics and increment logic

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) readServerHits(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load()))
+}
+func (cfg *apiConfig) resetServerHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Reset done"))
+
+}
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	cf := apiConfig{}
+	serveMux.Handle("/app/", cf.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	serveMux.HandleFunc("/healthz", (func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}))
+	serveMux.HandleFunc("/metrics/", cf.readServerHits)
+	serveMux.HandleFunc("/reset/", cf.resetServerHits)
 	httpServe := http.Server{Handler: serveMux, Addr: ":8080"}
-	if err := httpServe.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(httpServe.ListenAndServe())
 }
